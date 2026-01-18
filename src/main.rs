48850406c62fa0469f4be17b815ea1eaa7f0fb04
Napasorn <febriantowahyu63@gmail.com>
2026-01-18 21:37:34 +0700

    feat: simple to add arguments when use cargo run

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,16 @@
-use rand::Rng;
+// Interacting with Cargo
 
-/// This trait defines the struct to be a user.
-trait IsUser {
-    /// This function proclaims that the struct is a user.
-    ///
-    /// # Arguments
-    /// None
-    ///
-    /// # Returns
-    /// (bool) true if user, false if not
-    fn is_user() -> bool {
-        return true
-    }
-}
-/// This struct defines a user
-///
-/// # Attributes
-/// * name (String): the name of the user
-/// * age (i8): the age of the user
-struct User {
-    name: String,
-    age: i8
-}
-
-fn generate_float(rng: &mut impl Rng) -> f64 {
-    rng.random_range(0.0..50.0)
-}
-
+use std::env;
 fn main() {
-    let random_number = generate_float(&mut rand::rng());
-    println!("{}", random_number);
+    let args: Vec<String> =  env::args().collect();
+    let path: &str = &args[0];
+    if path.contains("/debug/") {
+        println!("Debug is running");
+    }
+    else if path.contains("/release/") {
+        println!("Release is running");
+    }
+    else {
+        panic!("The setting is neither debug or release");
+    }
 }
